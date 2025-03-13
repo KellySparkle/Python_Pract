dca5b71dde0903093bc5e3e737bb7f2f1f820d94
Ксения <118428154+KellySparkle@users.noreply.github.com>
2025-03-13 10:37:33 +0500

    Update Task 10.py

--- Циклы For и While/Task 10.py
+++ Циклы For и While/Task 10.py
@@ -9,7 +9,7 @@
 
 def find_divisors(n):
     divisors = []
-    for i in range(1, n + 1):
+    for i in range(1, n):  
         if n % i == 0:
             divisors.append(i)
     return divisors
@@ -31,22 +31,3 @@ def count_even_divisors(divisors):
 
 def count_divisors_greater_than_d(divisors, d):
     return sum(1 for divisor in divisors if divisor > d)
-
-# Ввод натурального числа
-n = int(input("Введите натуральное число: "))
-
-# Получение всех делителей
-divisors = find_divisors(n)
-
-# Вывод результатов
-print(f"а) Все делители числа {n}: {divisors}")
-print(f"б) Сумма всех делителей: {sum_of_divisors(divisors)}")
-print(f"в) Сумма четных делителей: {sum_of_even_divisors(divisors)}")
-print(f"г) Количество всех делителей: {count_divisors(divisors)}")
-print(f"д) Количество нечетных делителей: {count_odd_divisors(divisors)}")
-print(f"е) Количество всех делителей: {count_divisors(divisors)}")
-print(f"ж) Количество четных делителей: {count_even_divisors(divisors)}")
-
-# Ввод числа d для сравнения
-d = int(input("Введите число d для сравнения с делителями: "))
-print(f"ж) Количество делителей, больших {d}: {count_divisors_greater_than_d(divisors, d)}")
